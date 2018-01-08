fc8aafa641072133b0e1ec97a45777d1fd3700ff
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-01-08 23:14:03 +0000

    Try to find which array position was written to

--- meltdown.c
+++ meltdown.c
@@ -18,27 +18,29 @@ int main() {
 
 	array[PAGE_SIZE*15] = 123;
 
-	// // https://stackoverflow.com/a/14214220/2057521
-	asm volatile (
-			"cpuid\n\t"/*serialize*/
-			"rdtsc\n\t"/*read the clock*/
-			"mov %%edx, %0\n\t"
-			"mov %%eax, %1\n\t": "=r" (cycles_high_start), "=r"
-			(cycles_low_start):: "%rax", "%rbx", "%rcx", "%rdx");
+	for(i=0; i<256; i++) {
+		// // https://stackoverflow.com/a/14214220/2057521
+		asm volatile (
+				"cpuid\n\t"/*serialize*/
+				"rdtsc\n\t"/*read the clock*/
+				"mov %%edx, %0\n\t"
+				"mov %%eax, %1\n\t": "=r" (cycles_high_start), "=r"
+				(cycles_low_start):: "%rax", "%rbx", "%rcx", "%rdx");
 
-	asm volatile ("" : : "c"(array[PAGE_SIZE*15]));
+		asm volatile ("" : : "c"(array[PAGE_SIZE*i]));
 
-	asm volatile (
-			"rdtscp\n\t"/*read the clock*/
-			"mov %%edx, %0\n\t"
-			"mov %%eax, %1\n\t"
-			"cpuid\n\t": "=r" (cycles_high_end), "=r"
-			(cycles_low_end):: "%rax", "%rbx", "%rcx", "%rdx");
+		asm volatile (
+				"rdtscp\n\t"/*read the clock*/
+				"mov %%edx, %0\n\t"
+				"mov %%eax, %1\n\t"
+				"cpuid\n\t": "=r" (cycles_high_end), "=r"
+				(cycles_low_end):: "%rax", "%rbx", "%rcx", "%rdx");
 
-	cycles = (((unsigned long) cycles_high_end << 32) | cycles_low_end) -
-		((unsigned long) cycles_high_start << 32 | cycles_low_start);
+		cycles = (((unsigned long) cycles_high_end << 32) | cycles_low_end) -
+			((unsigned long) cycles_high_start << 32 | cycles_low_start);
 
-	printf("cycles %3d: %lu\n", i, cycles);
+		printf("cycles %3d: %lu\n", i, cycles);
+	}
 
 	return 0;
 }
