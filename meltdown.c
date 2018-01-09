2ce5df57e97fe69abefb7cd36dbcd44f4be28e73
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2018-01-09 08:56:18 +0000

    Access cache by reading and not writing
    Write into the memory only to make sure the compiler "can't" optimise
    reads by already knowing what's in the read position.
    
    Tested and seems to work. However sometimes the position with less
    access time is position 0. This makes sense since the remaining
    variables of the program fit on the same page as array[0] and therefore
    accessing the variables caches the lower part of the array.

--- meltdown.c
+++ meltdown.c
@@ -11,12 +11,21 @@ int main() {
 	unsigned int cycles_high_start, cycles_low_start,
 				 cycles_high_end, cycles_low_end;
 	unsigned long cycles;
+	unsigned long cycles_min = ~0;
 	int i;
+	int i_min = -1;
+	unsigned char dummy;
+
+	for(i=0; i<256; i++)
+		array[PAGE_SIZE*i] = i;
 
 	for(i=0; i<256; i++)
 		_mm_clflush(&array[PAGE_SIZE*i]);
 
-	array[PAGE_SIZE*15] = 123;
+	asm volatile ("cpuid\n\t" ::: "%rax", "%rbx", "%rcx", "%rdx");
+
+	dummy = array[PAGE_SIZE*15];
+	printf("dummy %d\n", (unsigned int) dummy);
 
 	for(i=0; i<256; i++) {
 		// // https://stackoverflow.com/a/14214220/2057521
@@ -27,7 +36,7 @@ int main() {
 				"mov %%eax, %1\n\t": "=r" (cycles_high_start), "=r"
 				(cycles_low_start):: "%rax", "%rbx", "%rcx", "%rdx");
 
-		asm volatile ("" : : "c"(array[PAGE_SIZE*i]));
+		dummy = array[PAGE_SIZE*i];
 
 		asm volatile (
 				"rdtscp\n\t"/*read the clock*/
@@ -39,8 +48,17 @@ int main() {
 		cycles = (((unsigned long) cycles_high_end << 32) | cycles_low_end) -
 			((unsigned long) cycles_high_start << 32 | cycles_low_start);
 
-		printf("cycles %3d: %lu\n", i, cycles);
+		// printf("cycles %3d: %lu\n", i, cycles);
+
+		if(cycles <= cycles_min) {
+			cycles_min = cycles;
+			i_min = i;
+		}
+
 	}
+
+	printf("cycles %3d: %lu\n", i_min, cycles_min);
+	printf("dummy %d\n", (unsigned int) dummy);
 
 	return 0;
 }
